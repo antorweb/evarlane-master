ed6643bd7de4c33c02b1c7b8c1dc8c2b4bae596d
antorweb <antorremeni@gmail.com>
2025-11-19 10:08:34 +0600

    chnage

--- app/components/restorent/InfoSection.jsx
+++ app/components/restorent/InfoSection.jsx
@@ -1,95 +1,158 @@
 export default function InfoSection() {
   return (
+    <section className="InfoSection ">
+      <div className="container">
+        <div className=" rounded-xl overflow-hidden bg-[#FBFBFB] shadow  ">
+          <div className="flex justify-center  ">
+            {/* LEFT — Delivery Information */}
+            <div className="w-[496px] h-[659px] hover:bg-black">
+              <h2 className=" text-[32px] font-bold font-main mb-[42px] text-[#03081F]">
+                <span>🚚</span> Delivery information
+              </h2>
 
+              <ul className="space-y-5">
+                <li className="text-[18px] font-normal font-main text-[#000]">
+                  <strong className="text-[18px] font-bold font-main text-[#03081F]">
+                    Monday:
+                  </strong>{" "}
+                  12:00 AM–3:00 AM, 8:00 AM–3:00 AM
+                </li>
+                <li className="text-[18px] font-normal font-main text-[#000]">
+                  <strong className="text-[18px] font-bold font-main text-[#03081F]">
+                    Tuesday:
+                  </strong>{" "}
+                  8:00 AM–3:00 AM
+                </li>
+                <li className="text-[18px] font-normal font-main text-[#000]">
+                  <strong className="text-[18px] font-bold font-main text-[#03081F]">
+                    Wednesday:
+                  </strong>{" "}
+                  8:00 AM–3:00 AM
+                </li>
+                <li className="text-[18px] font-normal font-main text-[#000]">
+                  <strong className="text-[18px] font-bold font-main text-[#03081F]">
+                    Thursday:
+                  </strong>{" "}
+                  8:00 AM–3:00 AM
+                </li>
+                <li className="text-[18px] font-normal font-main text-[#000]">
+                  <strong className="text-[18px] font-bold font-main text-[#03081F]">
+                    Friday:
+                  </strong>{" "}
+                  8:00 AM–3:00 AM
+                </li>
+                <li className="text-[18px] font-normal font-main text-[#000]">
+                  <strong className="text-[18px] font-bold font-main text-[#03081F]">
+                    Saturday:
+                  </strong>{" "}
+                  8:00 AM–3:00 AM
+                </li>
+                <li className="text-[18px] font-normal font-main text-[#000]">
+                  <strong className="text-[18px] font-bold font-main text-[#03081F]">
+                    Sunday:
+                  </strong>{" "}
+                  8:00 AM–12:00 AM
+                </li>
+              </ul>
 
+              <p className="text-sm mt-3">
+                <strong>Estimated time until delivery:</strong> 20 min
+              </p>
+            </div>
 
+            {/* CENTER — Contact Information */}
+            <div className="w-[496px] h-[659px] hover:bg-black ">
+              <h2 className=" text-[32px] font-bold mb-[42px] font-main text-[#03081F]">
+                <span>📇</span> Contact information
+              </h2>
 
+              <p className="text-[18px] font-normal font-main text-black">
+                If you have allergies or other dietary
+              </p>
+              <div className="w-[427px] mt-[10px]">
+                <p className="text-[18px] font-normal font-main text-black">
+                  restrictions, please contact the restaurant. The restaurant
+                  will provide food-specific information upon request.
+                </p>
+              </div>
+              <p className="text-sm mt-4">
+                <strong className="text-[18px] font-bold font-main text-black">
+                  Phone number
+                </strong>
+                <br />{" "}
+                <p className="text-[24px] font-normal font-main text-[#03081F]">
+                  +934443-43
+                </p>
+              </p>
 
-    <section className="InfoSection mt-[23px] mb-[98px]" >
+              <p className="mt-[14px]">
+                <strong className="text-[18px] font-bold font-main text-black">
+                  Website
+                </strong>
+                <br />
+                <a
+                  href="http://mcdonalds.uk/"
+                  className="text-[24px] font-normal pt-[5px] font-main text-[#03081F] underline"
+                >
+                  http://mcdonalds.uk/
+                </a>
+              </p>
+            </div>
 
-        <div className="container">
+            {/* RIGHT — Operational Times */}
+            <div className="w-[496px] h-[659px] hover:bg-black">
+              <h2 className=" text-[32px] font-bold font-main mb-[42px] text-[#03081F]">
+                <span>🕒</span> Operational Times
+              </h2>
 
-
-
-
-      <div className=" rounded-xl overflow-hidden bg-[#FBFBFB] shadow ">
-
-<div className="flex justify-center gap-[63px] py-[122px] pb-[107px]">
-    
-
-        {/* LEFT — Delivery Information */}
-        <div className="">
-          <h2 className=" text-[32px] font-bold font-main mb-[42px] text-[#03081F]">
-            <span>🚚</span> Delivery information
-          </h2>
-
-          <ul className="space-y-5">
-            <li className="text-[18px] font-normal font-main text-[#000]"><strong className="text-[18px] font-bold font-main text-[#03081F]">Monday:</strong > 12:00 AM–3:00 AM, 8:00 AM–3:00 AM</li>
-            <li className="text-[18px] font-normal font-main text-[#000]"><strong className="text-[18px] font-bold font-main text-[#03081F]">Tuesday:</strong > 8:00 AM–3:00 AM</li>
-            <li className="text-[18px] font-normal font-main text-[#000]"><strong className="text-[18px] font-bold font-main text-[#03081F]">Wednesday:</strong > 8:00 AM–3:00 AM</li>
-            <li className="text-[18px] font-normal font-main text-[#000]"><strong className="text-[18px] font-bold font-main text-[#03081F]">Thursday:</strong > 8:00 AM–3:00 AM</li>
-            <li className="text-[18px] font-normal font-main text-[#000]"><strong className="text-[18px] font-bold font-main text-[#03081F]">Friday:</strong > 8:00 AM–3:00 AM</li>
-            <li className="text-[18px] font-normal font-main text-[#000]"><strong className="text-[18px] font-bold font-main text-[#03081F]">Saturday:</strong > 8:00 AM–3:00 AM</li>
-            <li className="text-[18px] font-normal font-main text-[#000]"><strong className="text-[18px] font-bold font-main text-[#03081F]">Sunday:</strong > 8:00 AM–12:00 AM</li>
-          </ul>
-
-          <p className="text-sm mt-3">
-            <strong>Estimated time until delivery:</strong> 20 min
-          </p>
+              <ul className=" space-y-5">
+                <li className="text-[18px] font-normal font-main text-[#000]">
+                  <strong className="text-[18px] font-bold font-main text-[#03081F]">
+                    Monday:
+                  </strong>{" "}
+                  8:00 AM–3:00 AM
+                </li>
+                <li className="text-[18px] font-normal font-main text-[#000]">
+                  <strong className="text-[18px] font-bold font-main text-[#03081F]">
+                    Tuesday:
+                  </strong>{" "}
+                  8:00 AM–3:00 AM
+                </li>
+                <li className="text-[18px] font-normal font-main text-[#000]">
+                  <strong className="text-[18px] font-bold font-main text-[#03081F]">
+                    Wednesday:
+                  </strong>{" "}
+                  8:00 AM–3:00 AM
+                </li>
+                <li className="text-[18px] font-normal font-main text-[#000]">
+                  <strong className="text-[18px] font-bold font-main text-[#03081F]">
+                    Thursday:
+                  </strong>{" "}
+                  8:00 AM–3:00 AM
+                </li>
+                <li className="text-[18px] font-normal font-main text-[#000]">
+                  <strong className="text-[18px] font-bold font-main text-[#03081F]">
+                    Friday:
+                  </strong>{" "}
+                  8:00 AM–3:00 AM
+                </li>
+                <li className="text-[18px] font-normal font-main text-[#000]">
+                  <strong className="text-[18px] font-bold font-main text-[#03081F]">
+                    Saturday:
+                  </strong>{" "}
+                  8:00 AM–3:00 AM
+                </li>
+                <li className="text-[18px] font-normal font-main text-[#000]">
+                  <strong className="text-[18px] font-bold font-main text-[#03081F]">
+                    Sunday:
+                  </strong>{" "}
+                  8:00 AM–3:00 AM
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-
-        {/* CENTER — Contact Information */}
-        <div className=" ">
-          <h2 className=" text-[32px] font-bold mb-[42px] font-main text-[#03081F]">
-            <span>📇</span> Contact information
-          </h2>
-
-          <p className="text-[18px] font-normal font-main text-black">If you have allergies or other dietary</p>
-<div className="w-[427px] mt-[10px]">
-
-          <p className="text-[18px] font-normal font-main text-black">
-             restrictions, please
-            contact the restaurant. The restaurant will provide food-specific
-            information upon request.
-          </p>
-
-</div>
-          <p className="text-sm mt-4">
-            <strong className="text-[18px] font-bold font-main text-black">Phone number</strong><br /> <p className="text-[24px] font-normal font-main text-[#03081F]">+934443-43</p>
-          </p>
-
-          <p className="mt-[14px]">
-            <strong className="text-[18px] font-bold font-main text-black">Website</strong><br />
-            <a href="http://mcdonalds.uk/" className="text-[24px] font-normal pt-[5px] font-main text-[#03081F] underline">
-              http://mcdonalds.uk/
-            </a>
-          </p>
-        </div>
-
-        {/* RIGHT — Operational Times */}
-        <div className="">
-          <h2 className=" text-[32px] font-bold font-main mb-[42px] text-[#03081F]">
-            <span>🕒</span> Operational Times
-          </h2>
-
-          <ul className=" space-y-5">
-            <li className="text-[18px] font-normal font-main text-[#000]"><strong className="text-[18px] font-bold font-main text-[#03081F]">Monday:</strong > 8:00 AM–3:00 AM</li>
-            <li className="text-[18px] font-normal font-main text-[#000]"><strong className="text-[18px] font-bold font-main text-[#03081F]">Tuesday:</strong > 8:00 AM–3:00 AM</li>
-            <li className="text-[18px] font-normal font-main text-[#000]"><strong className="text-[18px] font-bold font-main text-[#03081F]">Wednesday:</strong > 8:00 AM–3:00 AM</li>
-            <li className="text-[18px] font-normal font-main text-[#000]"><strong className="text-[18px] font-bold font-main text-[#03081F]">Thursday:</strong > 8:00 AM–3:00 AM</li>
-            <li className="text-[18px] font-normal font-main text-[#000]"><strong className="text-[18px] font-bold font-main text-[#03081F]">Friday:</strong > 8:00 AM–3:00 AM</li>
-            <li className="text-[18px] font-normal font-main text-[#000]"><strong className="text-[18px] font-bold font-main text-[#03081F]">Saturday:</strong > 8:00 AM–3:00 AM</li>
-            <li className="text-[18px] font-normal font-main text-[#000]"><strong className="text-[18px] font-bold font-main text-[#03081F]">Sunday:</strong > 8:00 AM–3:00 AM</li>
-          </ul>
-        </div>
-
-</div>
-
-
       </div>
-
-        </div>
-
     </section>
   );
 }
